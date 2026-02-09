175f750892465175ccde64fcf6a121abc2008843
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-02-09 18:41:56 -0300

    feat: agregar funcionalidad de notificaciones con estilos y contenedor

---
+++ notistack.js/notistack.js
@@ -0,0 +1,63 @@
+import './notistack.css';
+
+export let container;
+
+window.addEventListener('DOMContentLoaded', createNotificationContainer);
+
+export function createNotificationContainer(options = {}) {
+  if (!container) {
+    container = document.getElementById('notistack-container');
+    if (!container) {
+      container = document.createElement('div');
+      container.id = 'notistack-container';
+      container.className = 'notistack-container';
+      options.style && (container.style = options.style);
+      document.body.appendChild(container);
+    }
+  }
+
+  return container;
+}
+
+export function pushNotification(message, options = {}) {
+  if (typeof options === 'string') {
+    options = { variant: options };
+  }
+
+  const variant = options.variant;
+  switch (variant) {
+    case 'warning':
+      console.warn(message);
+      break;
+
+    case 'error':
+      console.error(message);
+      break;
+
+    case 'info':
+      console.info(message);
+      break;
+
+    case 'debug':
+      console.debug(message);
+      break;
+
+    case 'success':
+    default:
+      console.log(message);
+  }
+  
+  const notification = document.createElement('div');
+  notification.classList.add('notification');
+  options.className && notification.classList.add(options.className);
+  options.classList?.length && notification.classList.add(...options.classList);
+  variant && notification.classList.add(variant);
+  options.style && (notification.style = options.style);
+  notification.innerText = message;
+
+  notification.addEventListener('click', () => notification.remove());
+  setTimeout(() => notification.remove(), options.duration || 5000);
+
+  container.appendChild(notification);
+}
+
