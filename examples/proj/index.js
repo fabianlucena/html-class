7ffb1c9dc5182c4f3868f361b006e054cb4bc31d
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-02-09 22:49:35 -0300

    feat: add proj example

---
+++ examples/proj/index.js
@@ -0,0 +1,18 @@
+import Proj from './src/proj/proj';
+import GanttScreen from './src/proj/gantt-screen';
+import CardsScreen from './src/proj/cards-screen';
+import { loadLanguage } from './src/locale';
+
+document.addEventListener('DOMContentLoaded', async () => {
+  await loadLanguage();
+  init();
+});
+
+function init() {
+  const proj = new Proj({ parent: 'app' });
+  proj.addScreen(new CardsScreen());
+  proj.addScreen(new GanttScreen());
+
+  proj.addIssue({ title: 'Issue 1', description: 'This is the first issue.', labels: ['bug', 'high priority'], dueDate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000) });
+  proj.addIssue({ title: 'Issue 2', description: 'This is the second issue.' });
+}
