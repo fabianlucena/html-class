3b55acf59be19b4f0129878f43e628aad072577f
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-03-11 16:48:13 -0300

    feat: update accepts property and define shape for ConnectorUtp for ActDia Nodes

--- components/actdia-nodes/internet/connector-utp.js
+++ components/actdia-nodes/internet/connector-utp.js
@@ -3,7 +3,19 @@ export default async function create({ actdia, Connector }) {
 
   return class ConnectorUtp extends Connector {
     type = 'utp';
-    accepts = [ 'in' ];
+    accepts = [ 'utp' ];
     multiple = false;
+
+    autoRotate = false;
+    shape = {
+      children: [
+        {
+          shape: 'path',
+          sx: .5,
+          sy: .5,
+          d: `M -.5 -.35 h 1 v .5 h -.25 v .2 h -.5 v -.2 h -.25 z`,
+        },
+      ],
+    };
   };
 }
