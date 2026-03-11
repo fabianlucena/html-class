6bfe3b3bcd0d120c56719217d6e51870bebe15f0
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-03-11 17:50:00 -0300

    feat: add localization function parameter to create method in InternetRouter node of ActDia

--- components/actdia-nodes/internet/router.js
+++ components/actdia-nodes/internet/router.js
@@ -1,4 +1,4 @@
-export default async function create({ actdia, Node }) {
+export default async function create({ actdia, Node, _f }) {
   await actdia.loadLocaleForMeta(import.meta);
 
   return class InternetRouter extends Node {
