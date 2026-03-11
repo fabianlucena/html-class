e4179cfe20c993833e600533b5a122c8a971c01e
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-03-11 18:21:44 -0300

    feat: add localization for PC nodefor ActDia

--- components/actdia-nodes/internet/locale/es.js
+++ components/actdia-nodes/internet/locale/es.js
@@ -2,6 +2,8 @@ export default {
   'Internet': 'Internet',
   'Internet router Node': 'Nodo de router de internet',
   'Internet switch Node': 'Nodo de switch de internet',
+  'PC': 'PC',
+  'PC node': 'Nodo de PC',
   'Ports': 'Puertos',
   'Router': 'Router',
   'Switch': 'Switch',
