3f09c9f0ad95bc231c60a04cd44c53349df87a87
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-03-12 20:27:36 -0300

    feat: implement setStatus and propagate methods for improved status handling in ConnectorIO in ActDia

--- components/actdia/connector-io.js
+++ components/actdia/connector-io.js
@@ -1,7 +1,50 @@
 import Connector from './connector.js';
+import { deepCopy } from '../utils/object.js';
 
 export default class ConnectorIO extends Connector {
   type = 'io';
   accepts = [ 'io', 'in', 'out' ];
   multiple = true;
+  isInput = true;
+  isOutput = true;
+
+  setStatus(status, options = {}) {
+    if (!status.send) {
+      if (typeof status !== 'object')
+        status = { send: status };
+      else 
+        status = { recv: Object.values(status || {})[0] };
+    }
+
+    status = deepCopy(status)
+    this.status = status;
+
+    if (this.onUpdate) {
+      this.onUpdate({ status });
+    }
+
+    this.item?.updateStatus({ ...options, connector: this });
+
+    this.propagate(options);
+  }
+
+  propagate(options = {}) {
+    options ??= {};
+    const connectors = new Set([...options.connectors || []]);
+    if (connectors?.has(this)) {
+      this.actdia?.pushNotification(_('Circular propagation detected, stopping.'), 'warning');
+      return;
+    }
+
+    const name = this.item.name + ' - ' + this.name;
+    const status = {
+      [name]: this.status.send,
+    };
+
+    this.connections.forEach(connection => {
+      options.from = this;
+      options.connectors = new Set([...connectors, this]);
+      connection.setStatus(status, options);
+    });
+  }
 }
