d778e3593e9c2237b5a14189078a54d8a850b994
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-03-14 02:17:13 -0300

    fix: remove redundant IP address assignments in network interface initialization

--- components/actdia-nodes/internet/pc_base.js
+++ components/actdia-nodes/internet/pc_base.js
@@ -35,16 +35,12 @@ export default async function create({ actdia }) {
 
     init() {
       super.init(...arguments);
-      this.netInterfaces = [];
-
       if (!this.getNetInterface('lo')) {
         this.addNetInterface({ name: 'lo', link: 'loopback' });
       }
 
       if (!this.getNetInterface('enp3s0')) {
-        let enp3s0 = this.addNetInterface({ name: 'enp3s0', connector: this.getConnector('enp3s0') });
-        this.addIPAddress(enp3s0, '192.168.0.15/24');
-        this.addIPAddress(enp3s0, 'fe80::3e97:eff:fe12:abcd/64');
+        this.addNetInterface({ name: 'enp3s0', connector: this.getConnector('enp3s0') });
       }
     }
   };
