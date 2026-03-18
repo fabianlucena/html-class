9e97485007d1c05e28e424571d6e0a4849adceda
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-03-18 10:55:11 -0300

    feat: add length getter to PacketPayload class for raw data length retrieval

--- components/internet/packet_payload.js
+++ components/internet/packet_payload.js
@@ -1,6 +1,10 @@
 import Icmp from './icmp4.js';
 
 export default class PacketPayload {
+  get length() {
+    return this.raw.length;
+  }
+  
   static createFromRaw({ protocol, raw }) {
     if (protocol === 1) {
       return Icmp.createFromRaw({ raw });
