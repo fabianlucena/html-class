9133dc1238b063e2ce16c7f43818bcc89ff3b393
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-03-25 11:41:39 -0300

    feat: remove debug log from createIPv6PacketPayload function

--- components/internet/ipv6_packet_payload_creator.js
+++ components/internet/ipv6_packet_payload_creator.js
@@ -1,7 +1,6 @@
 import createIcmp6 from './icmp6_creator.js';
 
 export default function createIPv6PacketPayload({ packet, raw }) {
-  console.log(packet.toString());
   switch (packet.nextHeader) {
     case 58: // ICMPv6
       return createIcmp6({ raw });
