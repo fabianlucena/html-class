2f891ec115002ff8256b547ccded202546097ab3
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-03-31 18:31:23 -0300

    feat: update LabelIOSelector shape on update of ActDia

--- components/actdia-nodes/label_io_selector.js
+++ components/actdia-nodes/label_io_selector.js
@@ -16,21 +16,19 @@ export default async function create({ actdia, Node }) {
       children: [
         {
           name: 'base',
-          x: -3,
-          y: -.5,
           shape: 'path',
           d: 'M .5 0 h 5 l .5 .5 l -.5 .5 v 1 h -5 v -1 l -.5 -.5 l .5 -.5 z',
         },
         {
           name: 'labels',
           shape: 'text',
-          x: -1.5,
-          y: -.5,
+          x: 1.5,
+          y: 0,
           width: 5,
           height: 3,
-          fontSize: .8,
+          fontSize: 1,
           lineSpacing: 1.0,
-          text: 'Label 11111111111111111\nLabel 2\nLabel 3',
+          text: 'Label 1\nLabel 2\nLabel 3',
           textAnchor: 'start',
           dominantBaseline: 'top',
           editable: true,
@@ -40,12 +38,14 @@ export default async function create({ actdia, Node }) {
           },
           onBlur: () => this.isEditing = false,
         },
+        {
+          name: 'buttons',
+          children: [],
+        },
       ],
     };
 
     box = {
-      x: -3,
-      y: -.5,
       width: 6,
       height: 2,
     };
@@ -63,6 +63,7 @@ export default async function create({ actdia, Node }) {
 
     #baseShape = null;
     #labelsShape = null;
+    #buttonsShape = null;
 
     get labels() {
       return this.#labelsShape.text;
@@ -80,16 +81,36 @@ export default async function create({ actdia, Node }) {
       super.init(...arguments);
       this.#baseShape = this.getShape('base');
       this.#labelsShape = this.getShape('labels');
+      this.#buttonsShape = this.getShape('buttons');
     }
     
     update() {
       const width = this.box.width;
-      const lines = Math.max(2, this.labels.split('\n').length);
+      const labels = this.labels.split('\n');
+      const labelsCount = labels.length;
+      const lines = Math.max(2, labelsCount);
       this.#baseShape.d = `M .5 0 h ${width - 1} l .5 .5 l -.5 .5 v ${lines} h -${width - 1} v -${lines} l -.5 -.5 l .5 -.5 z`;
       this.#labelsShape.width = width - 2;
       this.#labelsShape.height = lines;
-      this.box.height = lines;
-      this.nodeUpdate?.();
+      this.box.height = lines + 1;
+      this.io1.x = 0;
+      this.io1.y = .5;
+      this.io2.x = width;
+      this.io2.y = .5;
+
+      this.#buttonsShape.children = Array.from({ length: labelsCount }, (_, i) => ({
+        shape: 'circle',
+        name: `label-${i + 1}`,
+        x: 1,
+        y: .5 + i,
+        r: .4,
+      }));
+
+      this.nodeUpdate();
+    }
+
+    onClick({ shape }) {
+      console.log(shape);
     }
  };
 }
