a11323fa930059223a9900890f30b8546afaf565
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-05-11 13:31:47 -0300

    feat: refactor Base class to improve parent and element management with enhanced methods

--- components/utils/base.js
+++ components/utils/base.js
@@ -1,34 +1,33 @@
 export default class Base {
   #element = null;
   #parent = null;
+  #parentElement = null;
 
   constructor(options = {}) {
   }
 
-  get element() {
-    return this.#element;
+  get parentElement() {
+    return this.#parentElement;
   }
 
-  set element(value) {
+  set parentElement(value) {
     if (typeof value === 'string')
       value = document.getElementById(value);
 
-    this.#element = value;
-  }
-
-  get parent() {
-    return this.#parent;
-  }
-
-  set parent(value) {
-    if (typeof value === 'string')
-      value = document.getElementById(value);
-
-    this.#parent = value;
+    this.#parentElement = value;
   }
 
   create(options = {}) {
-    for (let [key, value] of Object.entries(options)) {
+    let { parent, parentElement, element, show, hide, display, className,...rest } = options;
+
+    this.parent = parent;
+    if (parentElement)
+      this.parentElement = parentElement;
+    this.element = element;
+
+    this.addClass(className);
+
+    for (let [key, value] of Object.entries(rest)) {
       const setter = `set${key[0].toUpperCase() + key.slice(1)}`;
       if (this[setter]) {
         this[setter](value);
@@ -37,5 +36,122 @@ export default class Base {
 
       this[key] = value; 
     }
+
+    if (show === false || hide)
+      this.hide();
+
+    if (typeof display !== 'undefined')
+      this.display = display;
+  }
+
+  destroy() {
+    this.element = null;
+  }
+
+  set parent(newParent) {
+    if (!newParent) {
+      this.#parent = null;
+      this.#parentElement = null;
+      this.#element?.remove();
+      return;
+    }
+
+    if (typeof newParent === 'string')
+      newParent = document.getElementById(newParent);
+
+    if (newParent instanceof HTMLElement) {
+      this.#parent = null;
+      this.parentElement = newParent;
+      return;
+    }
+
+    this.#parent = newParent;
+    if (this.#parent.#element instanceof HTMLElement)
+      this.parentElement = this.#parent.#element;
+  }
+
+  set parentElement(newParentElement) {
+    if (!newParentElement) {
+      this.#parent = null;
+      this.#parentElement = null;
+      this.#element?.remove();
+      return;
+    }
+
+    if (typeof newParentElement === 'string')
+      this.#parentElement = document.getElementById(newParentElement);
+
+    this.#parentElement = newParentElement;
+    if (this.#element)
+      this.#parentElement.appendChild(this.#element);
+  }
+
+  set element(newElement) {
+    if (!newElement) {
+      this.destroyElement();
+      return;
+    }
+
+    if (typeof newElement === 'string')
+      newElement = document.getElementById(newElement);
+
+    this.#element = newElement;
+    if (this.#parentElement)
+      this.#parentElement.appendChild(this.#element);
+  }
+
+  get element() {
+    return this.#element;
+  }
+
+  destroyElement() {
+    if (!this.#element)
+      return;
+
+    this.#element.remove();
+    this.#element = null;
+  }
+
+  addClass(...classNames) {
+    for (const className of classNames) {
+      if (!className)
+        return;
+
+      if (Array.isArray(className))
+        this.#element.classList.add(...className);
+      else if (className instanceof Set)
+        this.#element.classList.add(...className);
+      else if (typeof className === 'string')
+        this.#element.classList.add(...className.trim().split(/\s+/).filter((c) => c));
+      else if (typeof className === 'object') {
+        for (const key of className) {
+          let value = className[key];
+          if (!value)
+            continue;
+
+          if (typeof value === 'function')
+            value = value();
+
+          if (value)
+            this.#element.classList.add(key);
+        }
+      }
+    }
+  }
+
+  set display(value) {
+    this.#element.style.display = value ? '' : 'none';
+  }
+
+  get display() {
+    return this.#element.style.display !== 'none';
+  }
+  
+  hide() {
+    this.display = false;
+  }
+
+  show() {
+    this.display = true;
   }
 }
